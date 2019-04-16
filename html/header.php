b81d9ed4d50ef5da7309a0f43f08db8537c08c06
Egor <egorcloud57@gmail.com>
2019-04-16 15:43:37 +0300

    first commit

---
+++ html/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css" type="text/css">
+    <style>
+    h1{
+    text-align: center;
+}
+#conteiner{
+width: 800px;
+height: 500px;
+margin: 0 20% 0 20%;
+background-color: rgb(253, 169, 169);
+    }
+
+.row{
+width: 600px;
+height: 400px;
+margin: 0 100px 0 100px;
+background-color: rgb(23, 169, 169);
+}
+.block{
+float: left;
+}
+
+.block2{
+float: right;
+}
+
+    </style>
+    </head>
+    <body>
+
+    <div id="conteiner">
+    <h1>заголовки</h1>
+        <div class="row">
+       
+            <div class="block">
+            <h1>заголовки левые</h1>
+            </div>
+            <div class="block2">
+            <h1>заголовки правые</h1>
+            </div>
+        </div>
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+   error_reporting(-1);
+   session_start();
+   header("Content-Type:text/html;charset=utf8");
+?>
+
+
+<? include "html/header.php"; ?>
+<? include "html/footer.php"; ?>
